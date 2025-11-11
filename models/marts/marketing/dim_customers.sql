26ce4ca0bdd90f4443b8e707dd8d38bc44657fb8
Martin Hallberg <88983960+MartinJHallberg@users.noreply.github.com>
2025-11-11 16:53:29 +0100

    Total sum not correct

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -10,6 +10,31 @@ SELECT * FROM {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
+payment_sum as (
+    select 
+    order_id,
+        sum(amount) as total_payment_amount
+    from {{ ref('stg_stripe__payments') }}
+    group by 1
+),
+
+orders_with_payments as (
+    select 
+    o.*,
+    coalesce(ps.total_payment_amount, 0) as total_payment_amount
+    from orders o
+    left join payment_sum ps
+    on o.order_id = ps.order_id
+),
+
+customer_payments as (
+    select 
+    customer_id,
+        sum(total_payment_amount) as total_spent
+    from orders_with_payments
+    group by 1
+),
+
 customer_orders as (
 
     select
@@ -19,11 +44,14 @@ customer_orders as (
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
+
     from orders
 
     group by 1
 
 ),
+
+
 
 
 final as (
@@ -34,12 +62,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_payments.total_spent, 0) as total_spent
 
     from customers
 
     left join customer_orders using (customer_id)
-
+    left join customer_payments using (customer_id)
 )
 
-select * from final
+select sum(total_spent) from final
